34671e8d5ce4a67789e8d34babc5ed16d96db7cd
gemmapink <pinkpurplegemapple@gmail.com>
2024-02-11 20:49:50 -0500

    c

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-baker_street",
+      "name": "baker_street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3659731540677988,
+        "pitch": 0.2182249883276448,
+        "fov": 1.5470055774335754
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.9096156076341018,
+          "pitch": -0.12297183386807653,
+          "title": "Title",
+          "text": "<div>train station: The B&amp;O Railroad Station, built in 1886, fueled Aberdeen's growth for the seven decades it was in use. It facilitated the canning industry's expansion and served as a vital transportation hub during WWI and WWII, moving troops and supplies. Passenger service ceased in 1958, and the station was abandoned by the railroad in the early 1990s.</div><div><br></div><div>Click here to learn more!</div><div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
