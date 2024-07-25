0eb9595fde3e909114a8c831c842cb69e23a6ec9
gemmapink <156139625+gemmapink@users.noreply.github.com>
2024-07-25 09:35:07 -0400

    Update data.js

--- data.js
+++ data.js
@@ -35,6 +35,11 @@ var APP_DATA = {
         "pitch": -0.12297183386807653,
         "title": "Baker field Elementary",
 "text": "<div>Bakerfield Elementary School, located on <u> Baker Street </u> in Aberdeen, was built on a former farm <u>field </u> belonging to the C. W. <u> Baker family </u>, one of Aberdeen’s most prominent canners of sugar corn. </div><div><br></div><div>Click <a href='https://www.aberdeenmuseum.org/wp-content/uploads/2024/07/Bakerfield-Elementary.pdf' style='color:lightblue' target=\"_blank\">here </a> to learn more!</div>" }
+      {
+        "yaw": 0.95,
+        "pitch": -0.12297183386807653,
+        "title": "Baker field Elementary",
+"text": "<div>Bakerfield Elementary School, located on <u> Baker Street </u> in Aberdeen, was built on a former farm <u>field </u> belonging to the C. W. <u> Baker family </u>, one of Aberdeen’s most prominent canners of sugar corn. </div><div><br></div><div>Click <a href='https://www.aberdeenmuseum.org/wp-content/uploads/2024/07/Bakerfield-Elementary.pdf' style='color:lightblue' target=\"_blank\">here </a> to learn more!</div>" }
 ]
     }
   ],
